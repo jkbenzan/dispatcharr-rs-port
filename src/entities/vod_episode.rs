4457ed0a7e3a82acef7b1a6fd2e2bd077f7f2f26
jkbenzan <javier1281@yahoo.com>
2026-04-17 21:43:39 -0400

    Add VOD entities

---
+++ src/entities/vod_episode.rs
@@ -0,0 +1,28 @@
+use sea_orm::entity::prelude::*;
+use serde::{Deserialize, Serialize};
+
+#[derive(Clone, Debug, PartialEq, DeriveEntityModel, Serialize, Deserialize)]
+#[sea_orm(table_name = "vod_episode", schema_name = "public")]
+pub struct Model {
+    #[sea_orm(primary_key)]
+    pub id: i64,
+    pub uuid: Uuid,
+    pub name: String,
+    pub description: Option<String>,
+    pub air_date: Option<Date>,
+    pub rating: Option<String>,
+    pub duration_secs: Option<i32>,
+    pub season_number: Option<i32>,
+    pub episode_number: Option<i32>,
+    pub tmdb_id: Option<String>,
+    pub imdb_id: Option<String>,
+    pub custom_properties: Option<Json>,
+    pub created_at: DateTimeWithTimeZone,
+    pub updated_at: DateTimeWithTimeZone,
+    pub series_id: i64,
+}
+
+#[derive(Copy, Clone, Debug, EnumIter, DeriveRelation)]
+pub enum Relation {}
+
+impl ActiveModelBehavior for ActiveModel {}
